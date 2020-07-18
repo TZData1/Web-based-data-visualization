d37d7ab68df2fdbd4ddd0018fef04ad89304a168
TZData21 <51238639+TZData21@users.noreply.github.com>
2020-07-18 18:23:13 +0200

    [#2 Creating Simple Charts with vue-chartjs]

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/lumos_logo.svg">
+    <h1> Random numbers</h1>
+    <line-chart :data="chartData"></line-chart>
+    <line-chart :data="chartData"></line-chart>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      chartData: {
+        '01/2020': 10,
+        '02/2020': 20,
+        '03/2020': 10,
+        '04/2020': 40,
+        '05/2020': 50,
+        '06/2020': 10,
+        '07/2020': 70,
+        '08/2020': 40,
+        '09/2020': 100,
+        '010/2020': 90,
+      }
+    }
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -26,3 +41,8 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+<!-- 
+
+-->
--- application/src/components/HelloWorld.vue
+++ application/src/components/HelloWorld.vue
@@ -1,7 +1,55 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <h2>{{department}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    department : String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -27,32 +75,4 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+-->
